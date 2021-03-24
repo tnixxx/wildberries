5b674526253f16c367cf7d081e0d2b9e0261e7f7
tnixxx <tnixxx@yandex.ru>
2021-03-24 22:48:34 +0500

    Изучаю JS, с трудом, но получается

---
+++ js/main.js
@@ -0,0 +1,46 @@
+const mySwiper = new Swiper('.swiper-container', {
+	loop: true,
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.slider-button-next',
+		prevEl: '.slider-button-prev',
+	},
+});
+
+// cart
+
+const buttonCart = document.querySelector('.button-cart');
+const modalCart = document.querySelector('#modal-cart');
+const modalClose = document.querySelector('.modal-close');
+
+const openModal = function() {
+	modalCart.classList.add('show');
+};
+
+const closeModal = function() {
+	modalCart.classList.remove('show');
+};
+
+buttonCart.addEventListener('click', openModal);
+modalClose.addEventListener('click', closeModal);
+
+
+// sroll smooth
+
+{
+	const scrollLinks = document.querySelectorAll('a.scroll-link');
+  
+	for (const scrollLink of scrollLinks) {
+	  scrollLink.addEventListener('click', function(event) {
+		event.preventDefault();
+		const id = scrollLink.getAttribute('href');
+		document.querySelector(id).scrollIntoView({
+		  behavior: 'smooth',
+		  block: 'start',
+		});
+  
+	  });
+	}
+	
+  }
